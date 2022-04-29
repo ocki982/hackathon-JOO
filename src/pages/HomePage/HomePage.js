1e28c50e6f190cfbd27bb90713233955c26c587b
ocki982 <maiderocta@gmail.com>
2022-04-29 10:23:45 -0700

    fixed styles

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import DrinkDetails from '../../components/DrinkDetails/DrinkDetails';
-// import Hero from '../../components/Hero/Hero';
+import Hero from '../../components/Hero/Hero';
 import './HomePage.scss'
 
 class HomePage extends Component {
     render() {
         return (
         <div>
-            <DrinkDetails/>
+            <Hero/>
         </div>
         );
     }
